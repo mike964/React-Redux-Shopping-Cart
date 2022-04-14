46cfc2849fe8e05a8c070435445a1f7ced4fac14
Amen Kemal <57805638+mike964@users.noreply.github.com>
2022-04-14 23:02:14 +0300

    adding items to cart and count fixed

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -7,8 +7,8 @@ import Item6 from '../../images/item6.jpg'
 import {
 	ADD_TO_CART,
 	REMOVE_ITEM,
-	SUB_QUANTITY,
-	ADD_QUANTITY,
+	SUB_count,
+	ADD_count,
 	ADD_SHIPPING,
 } from '../actions/action-types/cart-actions'
 
@@ -61,89 +61,99 @@ const initState = {
 	shippingAddress: '',
 	total: 0,
 }
+
 const cartReducer = (state = initState, action) => {
-	//INSIDE HOME COMPONENT
-	if (action.type === ADD_TO_CART) {
-		// let addedItem = state.items.find(item => item.id === action.id)
-		// //check if the action id exists in the addedItems
-		// let existed_item = state.addedItems.find(item => action.id === item.id)
-		// if (existed_item) {
-		// 	addedItem.quantity += 1
+	switch (action.type) {
+		// * INSIDE HOME COMPONENT
+		case ADD_TO_CART:
+			let addedItem = state.items.find(item => item.id === action.id)
+			//check if the action id exists in the addedItems
+			// * If item already exists, just increase count by 1 : update item
+			let item_existed = state.addedItems.find(item => action.id === item.id)
+
+			let newItem = {
+				...addedItem,
+				count: 1,
+			}
+
+			if (item_existed) {
+				// * If
+				return {
+					...state,
+					addedItems: state.addedItems.map(item =>
+						item.id === action.id ? { ...item, count: item.count + 1 } : item
+					),
+				}
+			} else {
+				return {
+					...state,
+					addedItems: [...state.addedItems, newItem],
+					total: state.total + addedItem.price,
+				}
+			}
+		// }
+
+		// if (action.type === REMOVE_ITEM) {
+		// 	let itemToRemove = state.addedItems.find(item => action.id === item.id)
+		// 	let new_items = state.addedItems.filter(item => action.id !== item.id)
+
+		// 	//calculating the total
+		// 	let newTotal = state.total - itemToRemove.price * itemToRemove.count
+		// 	console.log(itemToRemove)
 		// 	return {
 		// 		...state,
-		// 		// total: state.total + addedItem.price,
+		// 		addedItems: new_items,
+		// 		total: newTotal,
 		// 	}
-		// } else {
-		// 	addedItem.quantity = 1
-		// 	//calculating the total
+		// }
+		// //INSIDE CART COMPONENT
+		// if (action.type === ADD_count) {
+		// 	let addedItem = state.items.find(item => item.id === action.id)
+		// 	addedItem.count += 1
 		// 	let newTotal = state.total + addedItem.price
 		// 	return {
 		// 		...state,
-		// 		// addedItems: [...state.addedItems, addedItem],
-		// 		// total: newTotal,
+		// 		total: newTotal,
+		// 	}
+		// }
+		// if (action.type === SUB_count) {
+		// 	let addedItem = state.items.find(item => item.id === action.id)
+		// 	//if the qt == 0 then it should be removed
+		// 	if (addedItem.count === 1) {
+		// 		let new_items = state.addedItems.filter(item => item.id !== action.id)
+		// 		let newTotal = state.total - addedItem.price
+		// 		return {
+		// 			...state,
+		// 			addedItems: new_items,
+		// 			total: newTotal,
+		// 		}
+		// 	} else {
+		// 		addedItem.count -= 1
+		// 		let newTotal = state.total - addedItem.price
+		// 		return {
+		// 			...state,
+		// 			total: newTotal,
+		// 		}
 		// 	}
 		// }
 
-		console.log('- action : ADD_TO_CART')
-	}
-	if (action.type === REMOVE_ITEM) {
-		let itemToRemove = state.addedItems.find(item => action.id === item.id)
-		let new_items = state.addedItems.filter(item => action.id !== item.id)
+		// if (action.type === ADD_SHIPPING) {
+		// 	return {
+		// 		...state,
+		// 		total: state.total + 6,
+		// 	}
+		// }
 
-		//calculating the total
-		let newTotal = state.total - itemToRemove.price * itemToRemove.quantity
-		console.log(itemToRemove)
-		return {
-			...state,
-			addedItems: new_items,
-			total: newTotal,
-		}
-	}
-	//INSIDE CART COMPONENT
-	if (action.type === ADD_QUANTITY) {
-		let addedItem = state.items.find(item => item.id === action.id)
-		addedItem.quantity += 1
-		let newTotal = state.total + addedItem.price
-		return {
-			...state,
-			total: newTotal,
-		}
-	}
-	if (action.type === SUB_QUANTITY) {
-		let addedItem = state.items.find(item => item.id === action.id)
-		//if the qt == 0 then it should be removed
-		if (addedItem.quantity === 1) {
-			let new_items = state.addedItems.filter(item => item.id !== action.id)
-			let newTotal = state.total - addedItem.price
-			return {
-				...state,
-				addedItems: new_items,
-				total: newTotal,
-			}
-		} else {
-			addedItem.quantity -= 1
-			let newTotal = state.total - addedItem.price
-			return {
-				...state,
-				total: newTotal,
-			}
-		}
-	}
-
-	if (action.type === ADD_SHIPPING) {
-		return {
-			...state,
-			total: state.total + 6,
-		}
-	}
-
-	if (action.type === 'SUB_SHIPPING') {
-		return {
-			...state,
-			total: state.total - 6,
-		}
-	} else {
-		return state
+		// if (action.type === 'SUB_SHIPPING') {
+		// 	return {
+		// 		...state,
+		// 		total: state.total - 6,
+		// 	}
+		// } else {
+		// 	return state
+		// }
+		default:
+			return state
 	}
 }
 
